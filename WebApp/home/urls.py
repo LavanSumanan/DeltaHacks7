24caad4ef1ab9acf2866c073e7db1a72d0e3f1c6
kcarnrite <kfcarnrite@gmail.com>
2021-03-07 15:49:48 +0000

    Added urls for monthly and weekly

--- WebApp/home/urls.py
+++ WebApp/home/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.home_view, name='home_view'),
-    path('learnmore/', views.learnMore_view, name='learnMore_view')
+    path('learnmore/', views.learnMore_view, name='learnMore_view'),
+    path('weekly/', views.weekly_view, name='weekly_view'),
+    path('monthly/', views.monthly_view, name='monthly_view')
 ]
