48d198ccd0912b73267996e676ff3a93cb5c53d6
kcarnrite <kfcarnrite@gmail.com>
2021-03-07 03:35:59 +0000

    Added home view

--- WebApp/home/views.py
+++ WebApp/home/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request):
     return HttpResponse("You're now logged in :)")
+
+def __BPInterpretation(systolic, diastolic):
+    if (systolic > 180) or (diastolic > 120):
+        return "Crisis"
+    elif (systolic >= 140) or (diastolic >= 90):
+        return "Stage 2"
+    elif (systolic >= 130 and systolic < 140) or (diastolic >= 80 and diastolic < 90):
+        return "Stage 1"
+    elif (systolic < 130 and systolic >= 120) and diastolic < 80:
+        return "Elevated"
+    elif systolic < 120 and diastolic < 80:
+        return "Normal"
